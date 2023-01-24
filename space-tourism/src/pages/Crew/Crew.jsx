d7d37d871aa2418c5ff2a3e3335ff203c8cf249a
Rudy Rivera <combustingkarma@gmail.com>
2023-01-24 16:42:15 -0600

    Crew page completed
    Crew page completed and made responsive

--- space-tourism/src/pages/Crew/Crew.jsx
+++ space-tourism/src/pages/Crew/Crew.jsx
@@ -17,16 +17,18 @@ export default function Crew() {
     <div className='crew'>
         <main className='crew__content container'>
             <h5 className='uppercase'><span className='numb'>02</span>meet your crew</h5>
-            <img src={images.png} alt='' aria-label={`image of ${role} ${name}`}/>
-            <hr />
-            <div>
-              <div className='controls'>
-                {crewControls}
-              </div>
-              <div className='crew__content__info'>
-                  <span className='eyebrow uppercase'>{role}</span>
-                  <h4 className='uppercase'>{name}</h4>
-                  <p>{bio}</p>
+            <div className='crew__content__item'>
+              <img src={images.png} alt='' aria-label={`image of ${role} ${name}`}/>
+              <hr />
+              <div className='crew__content__item__info'>
+                <div className='controls'>
+                  {crewControls}
+                </div>
+                <div className='crew__content__item__info__txt'>
+                    <span className='eyebrow uppercase'>{role}</span>
+                    <h4 className='uppercase'>{name}</h4>
+                    <p>{bio}</p>
+                </div>
               </div>
             </div>
         </main>
